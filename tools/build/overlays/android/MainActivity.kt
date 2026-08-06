d3b41ab2b444744727ec4a6db2b5cdaf90e65979
Paweł Potępa <potepapawel@gmail.com>
2026-08-07 01:19:54 +0200

    PORT-145: initialize native Android Keystore host

--- tools/build/overlays/android/MainActivity.kt
+++ tools/build/overlays/android/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.torca.app
 
+import android.os.Bundle
+import com.torca.host.AndroidKeystoreBridge
 import io.flutter.embedding.android.FlutterActivity
 
 /** Thin Android activity; product workflows live in the shared Flutter/Rust client. */
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    companion object {
+        init {
+            System.loadLibrary("torca_bridge")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidKeystoreBridge.initialize(applicationContext)
+        super.onCreate(savedInstanceState)
+    }
+}
