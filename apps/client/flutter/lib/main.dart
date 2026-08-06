91b0e1873d1de12a366a6b9ec00ccbc66907c033
Paweł Potępa <potepapawel@gmail.com>
2026-08-06 21:50:57 +0200

    bridge: default Flutter to native engine gateway

--- apps/client/flutter/lib/main.dart
+++ apps/client/flutter/lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/widgets.dart';
-import 'app.dart';
-import 'gateway/memory_engine_gateway.dart';
 
-void main() {
+import 'app.dart';
+import 'gateway/engine_gateway.dart';
+import 'gateway/memory_engine_gateway.dart';
+import 'gateway/method_channel_engine_gateway.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(TorcaApp(gateway: MemoryEngineGateway()));
+
+  const bool useMemoryGateway = bool.fromEnvironment(
+    'TORCA_USE_MEMORY_GATEWAY',
+    defaultValue: false,
+  );
+
+  final EngineGateway gateway;
+  if (useMemoryGateway) {
+    gateway = MemoryEngineGateway();
+  } else {
+    final MethodChannelEngineGateway nativeGateway =
+        MethodChannelEngineGateway();
+    await nativeGateway.initialize();
+    gateway = nativeGateway;
+  }
+
+  runApp(TorcaApp(gateway: gateway));
 }
