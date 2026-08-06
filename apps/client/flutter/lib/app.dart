662638939e092bc4a4a6410e8612bfab153d4eba
Paweł Potępa <potepapawel@gmail.com>
2026-08-07 00:07:28 +0200

    flutter: normalize shared app composition

--- apps/client/flutter/lib/app.dart
+++ apps/client/flutter/lib/app.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+
 import 'gateway/engine_gateway.dart';
 import 'screens/home_screen.dart';
 
 class TorcaApp extends StatelessWidget {
   const TorcaApp({required this.gateway, super.key});
+
   final EngineGateway gateway;
-  @override Widget build(BuildContext context) => MaterialApp(title: 'Torca', theme: ThemeData(colorSchemeSeed: Colors.blueGrey, useMaterial3: true), home: HomeScreen(gateway: gateway));
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Torca',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorSchemeSeed: Colors.blueGrey,
+          useMaterial3: true,
+        ),
+        home: HomeScreen(gateway: gateway),
+      );
 }
