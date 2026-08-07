c0aff6b37116bcf8f9a232b4de135098bc58a32c
Paweł Potępa <potepapawel@gmail.com>
2026-08-07 04:07:09 +0200

    PORT-210: add attachment insert command

---
+++ crates/infrastructure/torca-attachment-sqlite/sql/attachment_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO attachments(
+    attachment_id, message_id, name, media_type, size_bytes, status,
+    created_at_ms, updated_at_ms, attempt_count, transfer_offset, content_digest
+) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11);
