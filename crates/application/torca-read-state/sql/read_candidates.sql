b3fb0f997ed18b204db18721dabc7d1d39da180e
Paweł Potępa <potepapawel@gmail.com>
2026-08-07 10:51:40 +0200

    CLEANUP-258: remove read-state SQL from application crate

--- crates/application/torca-read-state/sql/read_candidates.sql
+++
@@ -1,7 +0,0 @@
-SELECT c.contact_id, m.message_id
-FROM conversations c
-JOIN messages m ON m.conversation_id = c.conversation_id
-WHERE c.conversation_id = ?1
-  AND m.direction = 1
-  AND m.status = 3
-ORDER BY m.created_at_ms, m.message_id;
