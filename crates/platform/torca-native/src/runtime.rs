25fa7b6b8d1f653eeb4e992b42239c2f1fa70832
Paweł Potępa <potepapawel@gmail.com>
2026-08-07 08:25:10 +0200

    PORT-222: activate process runtime registry

--- crates/platform/torca-native/src/runtime.rs
+++ crates/platform/torca-native/src/runtime.rs
@@ -4,7 +4,9 @@ mod composition;
 mod ffi;
 mod json;
 mod native_runtime;
+mod process_runtime;
 mod runtime_composition;
 
 pub use ffi::*;
 pub use native_runtime::NativeEngineRuntime;
+pub use process_runtime::NativeEngineHandle;
